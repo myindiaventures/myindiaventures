e5a8d6c8067c0d46b0ddd1f3dc8c07c9e13e9e37
JIG555ERA <jig111era@gmail.com>
2025-07-27 23:14:22 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import ComingSoon from './components/comingSoon/ComingSoon'
 import Home from './components/home/Home'
 import NotFound from './components/notFound/NotFound'
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   return (
