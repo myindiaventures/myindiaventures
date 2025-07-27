108d8b5782f2a22ba45ce2d2dde66108e7bab110
JIG555ERA <jig111era@gmail.com>
2025-07-27 17:53:25 +0530

    first commit

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { easeInOut, motion } from 'framer-motion'
+
+const Home = () => {
+
+    const screeenHeight = window.innerHeight;
+    const yoffset = screeenHeight * 0.4;
+
+  return (
+    <div
+    className='w-full h-screen bg-[#0E0E0E] flex justify-center items-center text-[#f9f9f9]'>
+      <motion.h1
+      initial={{ y:0, scale:2.5 }}
+      animate={{ y:-yoffset, scale:1}}
+      transition={{ delay:1, duration: 1.5, ease:easeInOut}}
+      className='font-[Sans] text-6xl font-semibold'>
+        My India Ventures
+      </motion.h1>
+    </div>
+  )
+}
+
+export default Home
