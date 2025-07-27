22aba38e51272dd7db80ac466594b4462dbbe64a
JIG555ERA <jig111era@gmail.com>
2025-07-27 22:40:21 +0530

    ..

--- client/src/components/comingSoon/Navbar.jsx
+++ client/src/components/comingSoon/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({active='home'}) => {
 
     return (
         <div
-        className='w-full h-[7vh] bg-[#f9f9f9]/10 inset-0 top-0 mt-[3vh] mx-auto rounded-4xl flex justify-between items-center lg:px-[24px] px-[16px]'>
+        className='lg:w-[800px] h-[70px] bg-[#f9f9f9]/10 inset-0 top-0 mt-[3vh] mx-auto rounded-4xl flex justify-between items-center lg:px-[24px] px-[16px]'>
             {/* brand logo */}
             <div
             className='font-[Pacifico] text-3xl'>
