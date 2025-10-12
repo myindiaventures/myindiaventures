b513dcd277fde91380f04eba41b12bb44a6bb309
JIG ERA <jig111era@gmail.com>
2025-10-12 19:50:04 +0530

    ..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import './index.css'
 import { App } from './App.jsx'
 import { BrowserRouter } from 'react-router-dom' 
 import { inject } from "@vercel/analytics";
+import { SpeedInsights } from '@vercel/analytics/react';
 
 inject();
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <SpeedInsights /> 
     <App />
   </BrowserRouter>
 )
