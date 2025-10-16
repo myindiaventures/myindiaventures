e2a0f7bd8bbda3863c4c785f690c3476e56c3d9b
JIG ERA <jig111era@gmail.com>
2025-10-16 16:52:48 +0530

    ..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <SpeedInsights /> 
+    <App />
     <Analytics />
     <SpeedInsights />
   </BrowserRouter>
